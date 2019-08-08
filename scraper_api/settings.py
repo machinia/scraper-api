4bca1d6df71d373458805ebc4b24c1b6e1d1eb47
Pablo Ahumada <pablo.ahumadadiaz@gmail.com>
2019-08-07 23:05:59 -0400

    Update the Django settings file
    To get the DEBUG and the SECRET_KEY settings from environment variables
    with default values. DEBUG will be True except when DJANGO_DEBUG=False.
    Also, the WSGI_APPLICATION setting has been created.

--- scraper_api/settings.py
+++ scraper_api/settings.py
@@ -1,3 +1,7 @@
-DEBUG = True
+import os
+
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 ROOT_URLCONF = "scraper_api.urls"
-SECRET_KEY = "(a(!1*6lt*n0-wdkof*e)nnrmun)zrlbgeuul!%d+b2!%m4-k_"
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY",
+                            "(a(!1*6lt*n0-wdkof*e)nnrmun)zrlbgeuul!%d+b2!%m4-k_")
+WSGI_APPLICATION = "scraper_api.wsgi.application"
