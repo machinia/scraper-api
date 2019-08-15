a46f29691c7712bd07a9e5117448355c86b5e671
Pablo Ahumada <pablo.ahumadadiaz@gmail.com>
2019-08-15 12:03:14 -0400

    Add the spider urls to the Django project urls

--- scraper_api/urls.py
+++ scraper_api/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path("heartbeat/", include("scraper_api.apps.heartbeat.urls")),
+    path("api/v1/spider/", include("scraper_api.apps.scraper.urls")),
 ]
