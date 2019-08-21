068b1427bb3f6f9c9d90e103d06233c1a17c857a
Pablo Ahumada <pablo.ahumadadiaz@gmail.com>
2019-08-21 17:25:46 -0400

    Update the scrap view unit tests

--- scraper_api/apps/scraper/test.py
+++ scraper_api/apps/scraper/test.py
@@ -17,24 +17,41 @@ class SpiderTest(SimpleTestCase):
         self.assertRedirects(resp, "/api/v1/spider/", 301, 200)
 
     def test_scrape_wrong_spider_name(self):
-        resp = self.client.post("/api/v1/spider/wrong-spider-name")
+        name = "wrong-spider-name"
+        resp = self.client.post("/api/v1/spider/%s" % name,
+                                content_type="application/json")
         self.assertEqual(resp.status_code, 404)
-        expected = {"error": "'wrong-spider-name': spider not found"}
+        expected = {"error": "Spider %s doesn't exist" % name}
         self.assertJSONEqual(resp.content, expected)
+
+    def test_scrape_no_data(self):
+        resp = self.client.get("/api/v1/spider/amazon-wishlist",
+                               content_type="application/json")
+        self.assertEqual(resp.status_code, 400)
+        self.assertJSONEqual(resp.content, {"error": "JSON data required"})
 
     def test_scrape_url_not(self):
         resp = self.client.post("/api/v1/spider/amazon-wishlist",
                                 data={},
                                 content_type="application/json")
         self.assertEqual(resp.status_code, 400)
-        self.assertJSONEqual(resp.content, {"error": "'url' key not found"})
+        error = "__init__() missing 1 required positional argument: 'url'"
+        self.assertJSONEqual(resp.content, {"error": error})
 
     def test_scrape_url_wrong(self):
         resp = self.client.post("/api/v1/spider/amazon-wishlist",
                                 data={"url": "wrong-url"},
                                 content_type="application/json")
-        self.assertEqual(resp.status_code, 200)
-        self.assertJSONEqual(resp.content, {"data": []})
+        self.assertEqual(resp.status_code, 400)
+        self.assertJSONEqual(resp.content, {"error": "Invalid URL"})
+
+    @mock.patch("scraper_factory.scrape", side_effect=RuntimeError("Test"))
+    def test_scrape_any_exception(self, scrape):
+        resp = self.client.post("/api/v1/spider/amazon-wishlist",
+                                data={"url": "some-url"},
+                                content_type="application/json")
+        self.assertEqual(resp.status_code, 500)
+        self.assertJSONEqual(resp.content, {"error": "Test"})
 
     @mock.patch("scraper_factory.scrape")
     def test_scrape_spider(self, scrape):
